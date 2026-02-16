9f2c94b4d1976a22b624a7522a26d2f837de804f
ernesto-VO <vo202463245@alm.buap.mx>
2026-02-15 21:41:04 -0600

    primer avanze

---
+++ genesis-city/mesh_instance_3d.gd
@@ -0,0 +1,13 @@
+extends MeshInstance3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	rotate_y(1.0*delta)	
+	scale.y = 1.0 + (sin(Time.get_ticks_msec()*0.005)*0.5)
+	pass
